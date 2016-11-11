fcb612d3f6d9b7ab5efbf4340d67d2c0db7bdc1b
DevChrisCross <Dev.ChrisMolina@gmail.com>
2016-11-11 18:16:36 +0800

    initial commit

---
+++ index.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>Styles Conference</title>
+		<meta charset="utf-8">
+		<link rel="stylesheet" type="text/css" href="assets/stylesheets/main.css">
+		 <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Lato:100,300,400">
+	</head>
+	<body>
+		<header class="primary-header container group">
+			<h1 class="logo">
+				<a href="index.php">Styles <br> Conference</a>
+			</h1>
+			<h3 class="tagline">August 24&ndash;26th &mdash; Chicago, IL</h3>
+			<nav class="nav primary-nav">
+				<ul>
+			    	<li><a href="index.php">Home</a></li><!--
+			    --><li><a href="speakers.php">Speakers</a></li><!--
+			    --><li><a href="schedule.php">Schedule</a></li><!--
+			    --><li><a href="venue.php">Venue</a></li><!--
+			    --><li><a href="register.php">Register</a></li>
+			  </ul>
+			</nav>
+		</header>
+		<section class="hero container">
+			<h2>Dedicated to the Craft of Building Websites</h2>
+			<p>Every year the brightest web designers and front-end developers descend on Chicago to discuss the latest technologies. Join us this August!</p>
+			<a href="register.php" class="btn btn-alt">Register Now</a>
+		</section>
+		<section class="row">
+			<div class="grid">
+				<section class="teaser col-1-3">
+				<h5>Speakers</h5>
+					<a href="speakers.php">
+						<img src="https://placeimg.com/640/480/any" alt="kolg">
+						<h3>World-Class Speakers</h3>
+					</a>
+					<p>Joining us from all around the world are over twenty fantastic speakers, here to share their stories.</p>
+				</section><!--
+				--><section class="teaser col-1-3"> 
+					<h5>Schedule</h5>
+					<a href="schedule.php">
+					<img src="https://placeimg.com/640/480/any" alt="kolg">
+						
+						<h3>Three Inspiring Days</h3>
+					</a>
+					<p>Enjoy three inspiring and action-packed days of talks, gatherings, and all-around good times.</p>
+				</section><!--
+				--><section class="teaser col-1-3">
+					<h5>Venue</h5>
+					<a href="venue.php">
+						<img src="https://placeimg.com/640/480/any" alt="kolg">
+						
+						<h3>The Chicago Theatre</h3>
+					</a>
+					<p>Withing the heart of downtown Chicago, The Chicago Theatre will provide a beatiful conference venue.</p>
+				</section>
+			</div>
+		</section>
+		<footer class="primary-footer container group">
+			<small>&copy; Styles Conference</small>
+			<nav class="nav">
+				<ul>
+			    	<li><a href="index.php">Home</a></li><!--
+			    --><li><a href="speakers.php">Speakers</a></li><!--
+			    --><li><a href="schedule.php">Schedule</a></li><!--
+			    --><li><a href="venue.php">Venue</a></li><!--
+			    --><li><a href="register.php">Register</a></li>
+			  </ul>
+			</nav>
+		</footer>
+	</body>
+</html>
---
+++ venue.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Styles Conference</title>
+		<meta charset="utf-8">
+		<link rel="stylesheet" type="text/css" href="assets/stylesheets/main.css">
+		 <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Lato:100,300,400">
+	</head>
+	<body>
+		<header class="primary-header container group">
+			<h1 class="logo">
+				<a href="index.php">Styles <br> Conference</a>
+			</h1>
+			<h3 class="tagline">August 24&ndash;26th &mdash; Chicago, IL</h3>
+			<nav class="nav primary-nav">
+				<ul>
+			    	<li><a href="index.php">Home</a></li><!--
+			    --><li><a href="speakers.php">Speakers</a></li><!--
+			    --><li><a href="schedule.php">Schedule</a></li><!--
+			    --><li><a href="venue.php">Venue</a></li><!--
+			    --><li><a href="register.php">Register</a></li>
+			  </ul>
+			</nav>
+		</header>
+		
+		<section class="row-alt">
+		 	<div class="lead container">
+			    <h1>Venue</h1>
+			    <p>We&#8217;re happy to welcome over twenty speakers to present on the industry&#8217;s latest technologies. Prepare for an inspiration extravaganza.</p>
+		  
+		  	</div>
+		</section>
+
+		<section class="row">
+		  <div class="grid">
+
+		    <section class="venue-theatre">
+    
+		      <div class="col-1-3">
+			    <h2>Chicago Theatre</h2>
+			    <p>175 N State St <br> Chicago, IL 60601</p>
+			    <p><a href="http://www.thechicagotheatre.com/">thechicagotheatre.com</a> <br> (312) 462-6300</p>
+			  </div><!--
+
+			  --><iframe class="venue-map col-2-3" src="https://www.google.com/maps/embed?pb=!1m5!3m3!1m2!1s0x880e2ca55810a493%3A0x4700ddf60fcbfad6!2schicago+theatre!5e0!3m2!1sen!2sus!4v1388701393606"></iframe>
+		    
+		    </section>
+		    
+		    <section class="venue-hotel">
+
+		      <div class="col-1-3"></div><!--
+		      --><iframe class="col-2-3"></iframe>
+		    
+		    </section>
+
+		  </div>
+		</section>
+
+
+
+		<footer class="primary-footer container group">
+			<small>&copy; Styles Conference</small>
+			<nav class="nav">
+				<ul>
+			    	<li><a href="index.php">Home</a></li><!--
+			    --><li><a href="speakers.php">Speakers</a></li><!--
+			    --><li><a href="schedule.php">Schedule</a></li><!--
+			    --><li><a href="venue.php">Venue</a></li><!--
+			    --><li><a href="register.php">Register</a></li>
+			  </ul>
+			</nav>
+		</footer>
+	</body>
+</html>
